3e6b61508b20eb137ab78317d3409f9744dbd767
John Colagioia <john@colagioia.net>
2020-12-23 06:01:12 -0500

    Set up initial structure

--- nono.js
+++ nono.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
 const im = require('imagemagick');
 
+if (process.argv.length < 3) {
+  console.log(`${process.argv[1]} requires an image file.`);
+  process.exit(1);
+}
 
+let imageFilename = process.argv[2];
