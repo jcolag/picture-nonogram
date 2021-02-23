9f9de0c5e84b2d38fa5a64a14a770f40c7c621b4
John Colagioia <john@colagioia.net>
2021-02-23 06:21:45 -0500

    Add (template) script to generate nightly puzzle

---
+++ nightly.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+today=$(date '+%Y%m%d')
+newdir="${HOME}/www/nono/${today}"
+mkdir "${newdir}"
+mv ${HOME}/www/nono/index.html "${newdir}"
+/usr/bin/node ${HOME}/apps/picture-nonogram/nono.js
+mv ${HOME}/apps/picture-nonogram/output.html ${HOME}/www/nono/index.html
+
